c26236cf54ac548f8b8917779fb342f026877267
Zygaross <147111835+Zygaross@users.noreply.github.com>
2023-10-09 15:23:28 +0000

    Updated help menu with sams goofy formatting

--- client/main.py
+++ client/main.py
@@ -121,10 +121,10 @@ def docommand(commandtext, *args):
 
 def help(*args):
   helplist = [
-      "/help ~ Display this text (/h)",
-      "/connect <username> ~ Connect to the server (c)",
-      "/join <room> ~ Join a room", "/users ~ Display online users (/j)",
-      "/rooms ~ Display info about rooms (/r)"
+      "/h, /help ~ Display this text (/h)",
+      "/c, /connect <username> ~ Connect to the server (c)",
+      "/j, /join <room> ~ Join a room", "/users ~ Display online users (/j)",
+      "/r, /rooms ~ Display info about rooms (/r)"
   ]
   return helplist
 
